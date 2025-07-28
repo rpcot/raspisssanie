2bdc8a45c5174285ef43b3cb46cc19c578ce2e9c
rpcot <rpcotmail@mail.ru>
2025-07-28 13:34:09 +0300

    edit cors

--- src/api/index.js
+++ src/api/index.js
@@ -114,14 +114,14 @@ const upload = multer();
 
 app.use(express.json());
 
-// app.use(cors({
-//     origin: 'https://schedule.rpcot.ru',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// }));
 app.use(cors({
-    origin: 'http://127.0.0.1:3000',
+    origin: 'https://schedule.rpcot.ru',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
+// app.use(cors({
+//     origin: 'http://127.0.0.1:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
 
 app.get('/', (req, res) => {
     res.status(200).json({ ok: true, status: 200, message: 'OK' });
